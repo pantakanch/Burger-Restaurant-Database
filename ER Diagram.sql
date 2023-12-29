f94a160ecde30ba776ac3a2808bd9a22947030c1
Pantakan Kangided <113978334+pantakanch@users.noreply.github.com>
2023-12-29 18:18:20 +0700

    Add files via upload

--- ER Diagram.sql
+++ ER Diagram.sql
@@ -1,45 +1,71 @@
-CREATE TABLE `customers` (
-  `customer_id` int PRIMARY KEY,
-  `firstname` text,
-  `lastname` text,
-  `address` text,
-  `email` text,
-  `phone` text
-);
+CREATE TABLE [customer] (
+  [customer_id] int PRIMARY KEY,
+  [firstname] text,
+  [lastname] text,
+  [address] text,
+  [email] text,
+  [phone] text
+)
+GO
 
-CREATE TABLE `invoice` (
-  `invoice_id` int PRIMARY KEY,
-  `order_date` Datetime,
-  `customer_id` int,
-  `menu_id` int,
-  `employee_id` int,
-  `payment_type` text
-);
+CREATE TABLE [receipt] (
+  [order_id] int PRIMARY KEY,
+  [order_date] Datetime,
+  [customer_id] int,
+  [menu_id] int,
+  [quantity] int,
+  [employee_id] int,
+  [payment_id] int
+)
+GO
 
-CREATE TABLE `orders` (
-  `menu_id` int PRIMARY KEY,
-  `customer_id` int,
-  `quantity` int
-);
+CREATE TABLE [menu_detail] (
+  [menu_id] int PRIMARY KEY,
+  [menu_name] text,
+  [price] int
+)
+GO
 
-CREATE TABLE `menu_detail` (
-  `menu_id` int PRIMARY KEY,
-  `name` text,
-  `size` text,
-  `price` int
-);
+CREATE TABLE [employee] (
+  [employee_id] int PRIMARY KEY,
+  [firstname] text,
+  [lastname] text,
+  [phone] text
+)
+GO
 
-CREATE TABLE `waiter` (
-  `employee_id` int PRIMARY KEY,
-  `firstname` text,
-  `lastname` text,
-  `phone` text
-);
+CREATE TABLE [payment] (
+  [payment_id] int PRIMARY KEY,
+  [payment_type] text
+)
+GO
 
-ALTER TABLE `invoice` ADD FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+CREATE TABLE [ingredient] (
+  [ing_id] INT PRIMARY KEY,
+  [ing_name] TEXT
+)
+GO
 
-ALTER TABLE `invoice` ADD FOREIGN KEY (`employee_id`) REFERENCES `waiter` (`employee_id`);
+CREATE TABLE [food_recipe] (
+  [menu_id] INT,
+  [ing_id] INT
+)
+GO
 
-ALTER TABLE `invoice` ADD FOREIGN KEY (`menu_id`) REFERENCES `orders` (`menu_id`);
+ALTER TABLE [receipt] ADD FOREIGN KEY ([customer_id]) REFERENCES [customer] ([customer_id])
+GO
 
-ALTER TABLE `orders` ADD FOREIGN KEY (`menu_id`) REFERENCES `menu_detail` (`menu_id`);
+ALTER TABLE [receipt] ADD FOREIGN KEY ([employee_id]) REFERENCES [employee] ([employee_id])
+GO
+
+ALTER TABLE [receipt] ADD FOREIGN KEY ([menu_id]) REFERENCES [menu_detail] ([menu_id])
+GO
+
+ALTER TABLE [receipt] ADD FOREIGN KEY ([payment_id]) REFERENCES [payment] ([payment_id])
+GO
+
+ALTER TABLE [menu_detail] ADD FOREIGN KEY ([menu_id]) REFERENCES [food_recipe] ([menu_id])
+GO
+
+ALTER TABLE [food_recipe] ADD FOREIGN KEY ([ing_id]) REFERENCES [ingredient] ([ing_id])
+GO
