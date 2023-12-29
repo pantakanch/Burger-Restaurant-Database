c02a01044e771f0ae2db47b2d59b2e803c968506
Pantakan Kangided <113978334+pantakanch@users.noreply.github.com>
2023-12-29 18:20:45 +0700

    Add files via upload

--- query_db_restuarant.sql
+++ query_db_restuarant.sql
@@ -1,101 +1,53 @@
--- create table customers
-CREATE TABLE customers (
-  customer_id int PRIMARY KEY,
-  firstname text,
-  lastname text,
-  address text,
-  email text,
-  phone text
-);
+-- 1. top-selling menu
+SELECT
+	md.menu_name,
+	SUM(r.quantity) AS total_sales
+FROM receipt r 
+JOIN menu_detail md ON r.menu_id = md.menu_id
+GROUP BY 1
+ORDER BY 2 DESC;
 
--- insert data into customers
-INSERT INTO customers(customer_id, firstname, lastname, address, email, phone) VALUES
-(1, 'John', 'Doe', '123 Main St', 'john.doe@email.com', '555-1234'),
-(2, 'Alice', 'Smith', '456 Oak Ave', 'alice.smith@email.com', '555-5678'),
-(3, 'Bob', 'Johnson', '789 Pine Ln', 'bob.johnson@email.com', '555-9876'),
-(4, 'Sarah', 'Williams', '321 Maple Dr', 'sarah.williams@email.com', '555-4321'),
-(5, 'David', 'Brown', '654 Cedar Blvd', 'david.brown@email.com', '555-8765'),
-(6, 'Emily', 'Davis', '987 Birch Rd', 'emily.davis@email.com', '555-2345'),
-(7, 'Michael', 'Miller', '135 Elm Ct', 'michael.miller@email.com', '555-6789'),
-(8, 'Jessica', 'Wilson', '246 Spruce Pl', 'jessica.wilson@email.com', '555-3456'),
-(9, 'Kevin', 'Lee', '579 Pinecrest Ave', 'kevin.lee@email.com', '555-7890'),
-(10, 'Olivia', 'Martin', '864 Oakwood Ln', 'olivia.martin@email.com', '555-4567');
+-- 2. sales by week 
+SELECT 
+	strftime('%W', order_date) AS week_number,
+	SUM((r.quantity * md.price)) AS total_sales
+FROM receipt r
+JOIN menu_detail md ON r.menu_id = md.menu_id
+GROUP BY 1;
 
--- create table invoice
-create table invoice (
-    invoice_id int PRIMARY KEY,
-    order_date Datetime,
-    customer_id int,
-	menu_id int,
-    employee_id int,
-	payment_type text
-);
+-- 3. top spending customer
+SELECT
+	r.customer_id,
+	c.firstname || ' ' || c.lastname AS full_name,
+	SUM((r.quantity *  md.price)) AS total_spending
+FROM receipt r
+JOIN menu_detail md ON r.menu_id = md.menu_id
+JOIN customer c ON r.customer_id = c.customer_id
+GROUP BY 1
+ORDER BY 3 DESC;
 
--- insert data into invoice
-INSERT INTO invoice(invoice_id, order_date, customer_id, menu_id, employee_id, payment_type) VALUES
-(1, '2023-01-15 08:30:00', 3, 1, 5, 'Credit Card'),
-(2, '2023-02-18 12:15:00', 7, 2, 2, 'Cash'),
-(3, '2023-03-22 17:45:00', 1, 3, 4, 'Debit Card'),
-(4, '2023-04-05 10:00:00', 9, 4, 2, 'Cash'),
-(5, '2023-05-12 14:20:00', 6, 5, 1, 'Credit Card'),
-(6, '2023-06-28 19:00:00', 4, 6, 3, 'Debit Card'),
-(7, '2023-07-08 21:45:00', 10, 7, 3, 'Cash'),
-(8, '2023-08-14 16:10:00', 2, 8, 1, 'Credit Card'),
-(9, '2023-09-19 11:30:00', 5, 9, 4, 'Debit Card'),
-(10, '2023-10-25 13:55:00', 8, 10, 5, 'Cash');
+-- 4. average order amount
+SELECT
+	AVG(quantity) AS avg_order_amount
+FROM receipt;
 
--- create table orders
-CREATE TABLE orders (
-  menu_id int PRIMARY KEY,
-  customer_id int,
-  quantity int
-);
+-- 5. employee who handle the most order
+SELECT
+	r.employee_id,
+	e.firstname || ' ' || e.lastname AS full_name,
+	COUNT(r.employee_id) AS no_order
+FROM receipt r
+JOIN employee e ON r.employee_id = e.employee_id
+GROUP BY 1
+ORDER BY 3 DESC;
 
--- insert data into orders
-INSERT INTO orders(menu_id, customer_id, quantity) VALUES 
-  (1, 3, 3),
-  (2, 7, 2),
-  (3, 1, 1),
-  (4, 9, 3),
-  (5, 6, 1),
-  (6, 4, 1),
-  (7, 10, 2),
-  (8, 2, 1),
-  (9, 5, 2),
-  (10, 8, 3);
-  
--- create table menu_detail
-CREATE TABLE menu_detail (
-  menu_id int PRIMARY KEY,
-  name text,
-  size text,
-  price int
-);
-
--- insert data into menu_detail
-INSERT INTO menu_detail(menu_id, name, size, price) VALUES 
-(1, 'Margherita Pizza', 'Medium', 12),
-(2, 'Chicken Alfredo Pasta', 'Large', 15),
-(3, 'Classic Burger', 'Regular', 8),
-(4, 'Vegetarian Wrap', 'Small', 10),
-(5, 'Grilled Chicken Salad', 'Large', 13),
-(6, 'Spaghetti Bolognese', 'Regular', 11),
-(7, 'Pepperoni Calzone', 'Medium', 14),
-(8, 'Caesar Salad', 'Small', 9),
-(9, 'BBQ Chicken Pizza', 'Large', 16),
-(10, 'Vegetable Stir Fry', 'Regular', 12);
-
--- create table waiter
-CREATE TABLE waiter (
-  employee_id int PRIMARY KEY,
-  firstname text,
-  lastname text,
-  phone text
-);
-
-INSERT INTO waiter(employee_id, firstname, lastname, phone) VALUES 
-(1, 'Alice', 'Johnson', '555-1234'),
-(2, 'Bob', 'Smith', '555-5678'),
-(3, 'Charlie', 'Williams', '555-9876'),
-(4, 'David', 'Brown', '555-4321'),
-(5, 'Emily', 'Davis', '555-8765');
+-- 6. what ingredients are used most frequently?
+SELECT
+    i.ing_id,
+    i.ing_name,
+    SUM(r.quantity) AS total_used
+FROM ingredient i
+JOIN food_recipe fr ON i.ing_id = fr.ing_id
+JOIN receipt r ON fr.menu_id = r.menu_id
+GROUP BY 1
+ORDER BY 3 DESC;
